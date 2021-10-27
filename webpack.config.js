1f4cc39700998dc643dd1ee6431c9bca3b0abddb
Carlos-SSH <carlos.anezeh@me.com>
2021-10-27 06:59:04 -0500

    Fix webpack issues.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: {
-    index: "./src/index.js",
+    index: "./src/app.js",
   },
   devtool: "inline-source-map",
   devServer: {
@@ -18,10 +18,9 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: "/",
   },
   module: {
     rules: [
