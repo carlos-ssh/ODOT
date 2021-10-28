1ddfc2b9e63c4c161b036710e175b728a7e0a04e
Carlos-SSH <carlos.anezeh@me.com>
2021-10-28 15:49:05 -0500

    functions refactor.

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import './style.css';
+import IsCompleted from './components/Status';
+
+let tasks = JSON.parse(localStorage.getItem('ToDo')) || [];
+
+const displayItems = () => {
+  const ul = document.getElementById('list');
+  tasks.forEach((item, index) => {
+    const CHECK = item.completed ? 'checked' : '';
+    const TROUGHLINE = item.completed ? 'line-through' : '';
+    item.index = index;
+    ul.innerHTML += `<li id="${item.index}"><input type="checkbox" class="checkbox" id="checkbox-${item.index}" ${CHECK}><input class="text ${TROUGHLINE} text-${item.index}" type="text" value ="${item.description}"><i class="fa fa-ellipsis-v open" aria-hidden="true"></i><i class="fa fa-trash-o trash d-none" aria-hidden="true"></i></li>`;
+  });
+  IsCompleted.completeToDo(tasks);
+  IsCompleted.changeIcon();
+};
+displayItems();
+
