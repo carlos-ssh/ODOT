fa7499b56cb0b7b4ae65b69965e4ae16a72185be
Carlos-SSH <carlos.anezeh@me.com>
2021-10-28 15:50:10 -0500

    create a function that displays tasks.

--- src/app.js
+++ src/app.js
@@ -16,3 +16,27 @@ const displayItems = () => {
 };
 displayItems();
 
+const clearItems = () => {
+  const ul = document.getElementById('list');
+  ul.innerHTML = '';
+};
+
+const addToTheList = () => {
+  const input = document.getElementById('input');
+  input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      const task = input.value;
+      if (task) {
+        const newTask = { description: task, completed: false, index: tasks.length };
+        tasks.push(newTask);
+        clearItems();
+        displayItems();
+        IsCompleted.updateLocalStorage(tasks);
+      }
+      input.value = '';
+      event.preventDefault();
+    }
+  });
+};
+addToTheList();
+
